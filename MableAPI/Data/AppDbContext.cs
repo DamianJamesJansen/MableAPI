915ab7e00055e791568f0ecf1bd1dd95e5240561
VKH\djansen <d.jansen@dynfos.nl>
2025-10-31 14:58:07 +0100

    add initial data.

--- MableAPI/Data/AppDbContext.cs
+++ MableAPI/Data/AppDbContext.cs
@@ -4,7 +4,7 @@ namespace MableAPI.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        private DbSet<Category> Categories => Set<Category>();
-        private DbSet<Product> Products => Set<Product>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
     }
 }
--- MableAPI/Program.cs
+++ MableAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MableAPI.Data;
+using System.Text.Json;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,14 +24,39 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
-
 //insert the initial data into the database
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    AppDbContext db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.EnsureCreated();
+
+    string pathCategory = Path.Combine(AppContext.BaseDirectory, "Data", "catergories.json");
+    string pathProduct = Path.Combine(AppContext.BaseDirectory, "Data", "articles.json");
+
+    //if empty, insert from json
+    if (File.Exists(pathCategory) && !db.Categories.Any())
+    {
+        string jsonCategories = await File.ReadAllTextAsync(pathCategory);
+        List<Category>? categories = JsonSerializer.Deserialize<List<Category>>(jsonCategories);
+        if (categories != null)
+        {
+            db.Categories.AddRange(categories);
+        }
+    }
+    
+    if (File.Exists(pathProduct) && !db.Products.Any())
+    {        
+        string jsonProducts = await File.ReadAllTextAsync(pathProduct);
+        List<Product>? products = JsonSerializer.Deserialize<List<Product>>(jsonProducts);
+        if (products != null)
+        {
+            //only add the products with a discount price of 100 or less
+            db.Products.AddRange(products.Where(p => p.DiscountPrice <= 100));
+        }
+        db.SaveChanges();
+    }
 }
 
+app.Run();
 //needed for WebApplicationFactory in tests
 public partial class Program { }
